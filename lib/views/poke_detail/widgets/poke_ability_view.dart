caedd2f5e3854b35d7ee7cc3b961c137fa4fd33d
ayush <ayushrestha05@gmail.com>
2023-12-15 14:58:10 +0545

    Removed SingleChildScroll from AbilityView

--- lib/views/poke_detail/widgets/poke_ability_view.dart
+++ lib/views/poke_detail/widgets/poke_ability_view.dart
@@ -12,51 +12,46 @@ class PokeAbilityView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return detail != null
-        //SingleChildScrollView is used to avoid overflow if the list of abilities is too long
-        ? SingleChildScrollView(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                  itemCount: detail!.abilities.length,
-                  itemBuilder: (ctx, i) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            detail!.abilities[i].name.toString().capitalize(),
-                            style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Consumer(
-                            builder: (context, ref, child) {
-                              final apiProvider = ref.read(apiNotifierProvider);
-                              return FutureBuilder(
-                                  future: apiProvider.getPokeAbilityDesc(
-                                      detail!.abilities[i].url.toString()),
-                                  builder: (ctx, snapshot) {
-                                    if (snapshot.connectionState ==
-                                        ConnectionState.done) {
-                                      return Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 8.0),
-                                        child: Text(
-                                          snapshot.data.toString(),
-                                          style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 12),
-                                        ),
-                                      );
-                                    } else {
-                                      return const SizedBox();
-                                    }
-                                  });
-                            },
-                          ),
-                          const Divider()
-                        ],
-                      )),
-            ),
+        ? Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: ListView.builder(
+                itemCount: detail!.abilities.length,
+                itemBuilder: (ctx, i) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          detail!.abilities[i].name.toString().capitalize(),
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Consumer(
+                          builder: (context, ref, child) {
+                            final apiProvider = ref.read(apiNotifierProvider);
+                            return FutureBuilder(
+                                future: apiProvider.getPokeAbilityDesc(
+                                    detail!.abilities[i].url.toString()),
+                                builder: (ctx, snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.done) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(top: 8.0),
+                                      child: Text(
+                                        snapshot.data.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.black, fontSize: 12),
+                                      ),
+                                    );
+                                  } else {
+                                    return const SizedBox();
+                                  }
+                                });
+                          },
+                        ),
+                        const Divider()
+                      ],
+                    )),
           )
         : const PokeErrorWidget();
   }
